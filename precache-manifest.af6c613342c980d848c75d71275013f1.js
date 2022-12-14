4508e141aa11c2a2821e7c8c0391c0e4713d6308
GitHub Action <Action@GitHub.com>
2022-12-14 15:46:31 +0800

    GitHub Actions Auto Builder at 2022-12-14 15:46:31

---
+++ precache-manifest.af6c613342c980d848c75d71275013f1.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969c55a8164f90d288ad726425414f55",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/CNAME"
+  },
+  {
+    "revision": "f9aaf9601986e2cf3148",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/app.3ae8ca61.css"
+  },
+  {
+    "revision": "9a79f542c0004f549779",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-02bec0c8.08fb1b07.css"
+  },
+  {
+    "revision": "60d2e0d9ddb2e43146d7",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-1680a564.13df863d.css"
+  },
+  {
+    "revision": "7a47dc7aa021f904b8f8",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-1dd9590f.03cf9eb5.css"
+  },
+  {
+    "revision": "416249d48efdf102df5a",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-264dd4c0.b7d9c5cf.css"
+  },
+  {
+    "revision": "98970a1cf763018ca529",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-2c3ad448.96d2bedc.css"
+  },
+  {
+    "revision": "107c560df7fd0b7d8c8b",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-39120e9f.e15c2cc7.css"
+  },
+  {
+    "revision": "45491c516e41fe19b8e7",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-5514c502.883a1ea7.css"
+  },
+  {
+    "revision": "c61be6059f0e23a4d7bc",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-5ae67ec0.95889dff.css"
+  },
+  {
+    "revision": "f117cc3054e717cf0364",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-5b1226ca.07b5e6fe.css"
+  },
+  {
+    "revision": "7828b85c4f40d9a24abc",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-66fe750d.48075486.css"
+  },
+  {
+    "revision": "9c4d52bf63377cc5f60b",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-6d02e1ac.f793d96f.css"
+  },
+  {
+    "revision": "dc3141aaab613aabc55a",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-6ef8816c.e9f6021c.css"
+  },
+  {
+    "revision": "82a2a69648b5388026a6",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-707d875c.3e9ecdb4.css"
+  },
+  {
+    "revision": "e0b2b110afeb64d41fc8",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-7f243bba.9c33be92.css"
+  },
+  {
+    "revision": "930b05f84a6c645ac05d",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-9818f064.813e590a.css"
+  },
+  {
+    "revision": "b43028dd30ee5ee8dfff",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-a66d016e.c6914b90.css"
+  },
+  {
+    "revision": "abef4bfe8b9f5b205831",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-b4aad398.6ad41d32.css"
+  },
+  {
+    "revision": "6ffd2f3da21a0d5d5273",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-dd93e0b2.15b75328.css"
+  },
+  {
+    "revision": "f73427f5af5d971c3de9",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-e322395a.1ad5c8c9.css"
+  },
+  {
+    "revision": "f16aeaf8ae32b2007167",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/css/chunk-vendors.aeecfed2.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "ccda47ec99576271f9f5bbbd02354679",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/index.html"
+  },
+  {
+    "revision": "f9aaf9601986e2cf3148",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/app.fcc3ce8d.js"
+  },
+  {
+    "revision": "9a79f542c0004f549779",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-02bec0c8.1fedc23d.js"
+  },
+  {
+    "revision": "60d2e0d9ddb2e43146d7",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-1680a564.d56f1b9c.js"
+  },
+  {
+    "revision": "a051df6be14b1d9fed0c3d7b4945e335",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-1680a564.d56f1b9c.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a47dc7aa021f904b8f8",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-1dd9590f.900cdf2d.js"
+  },
+  {
+    "revision": "817ad4647b87686eef3570d285495f05",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-1dd9590f.900cdf2d.js.LICENSE.txt"
+  },
+  {
+    "revision": "416249d48efdf102df5a",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-264dd4c0.e7c99dad.js"
+  },
+  {
+    "revision": "98970a1cf763018ca529",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-2c3ad448.e4b6bb75.js"
+  },
+  {
+    "revision": "2f92df5d60f460ca5db2",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-2d0b309a.bdf4d1ad.js"
+  },
+  {
+    "revision": "107c560df7fd0b7d8c8b",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-39120e9f.f515c372.js"
+  },
+  {
+    "revision": "45491c516e41fe19b8e7",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-5514c502.2f27a98b.js"
+  },
+  {
+    "revision": "c61be6059f0e23a4d7bc",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-5ae67ec0.789e7325.js"
+  },
+  {
+    "revision": "f117cc3054e717cf0364",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-5b1226ca.68f28783.js"
+  },
+  {
+    "revision": "7828b85c4f40d9a24abc",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-66fe750d.4b52a3ba.js"
+  },
+  {
+    "revision": "9c4d52bf63377cc5f60b",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-6d02e1ac.a005686b.js"
+  },
+  {
+    "revision": "dc3141aaab613aabc55a",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-6ef8816c.440e6b54.js"
+  },
+  {
+    "revision": "82a2a69648b5388026a6",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-707d875c.0d329c12.js"
+  },
+  {
+    "revision": "e0b2b110afeb64d41fc8",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-7f243bba.80052c1b.js"
+  },
+  {
+    "revision": "930b05f84a6c645ac05d",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-9818f064.d748aaa6.js"
+  },
+  {
+    "revision": "b43028dd30ee5ee8dfff",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-a66d016e.f0a21df6.js"
+  },
+  {
+    "revision": "abef4bfe8b9f5b205831",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-b4aad398.69a18004.js"
+  },
+  {
+    "revision": "6ffd2f3da21a0d5d5273",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-dd93e0b2.df919cc2.js"
+  },
+  {
+    "revision": "f73427f5af5d971c3de9",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-e322395a.343ca448.js"
+  },
+  {
+    "revision": "f16aeaf8ae32b2007167",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-vendors.7acff39a.js"
+  },
+  {
+    "revision": "8a74f08073689402c9d5cb3e39ca8218",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/js/chunk-vendors.7acff39a.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cdccc298e7b5c82893a4eedf21a17b",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jsdelivr.net/gh/isancha/isancha-admin-view-dist@master/robots.txt"
+  }
+]);
